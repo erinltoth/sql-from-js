0196289a591bb77d4687a8fc6cbce0fe2365efb8
Erin Toth <erintoth@gmail.com>
2019-01-29 20:43:56 +0000

    Add famous_people.js script to be able to query within famous_people table in test_db

---
+++ famous_people.js
@@ -0,0 +1,38 @@
+const pg = require('pg');
+
+const settings = require('./settings'); // settings.json
+
+const client = new pg.Client({
+    user    : settings.user,
+    password: settings.password,
+    database: settings.database,
+    host    : settings.hostname,
+    port    : settings.port,
+    ssl     : settings.ssl
+});
+
+const data = process.argv.slice(2)[0];
+
+client.connect();
+
+client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [data], (err, res) => {
+    console.log("Searching...");
+    const count = res.rows.length;
+    let num = 1;
+
+    
+    if (err) {
+        console.log('ERR:', err);
+        return false;
+    }
+    console.log(`Found ${count} person(s) by the name ${data}:`);
+    for (let i in res.rows) {
+        let date = res.rows[i].birthdate.getDate();
+        let month = res.rows[i].birthdate.getMonth() + 1;
+        let year = res.rows[i].birthdate.getFullYear();
+
+        console.log(`${num++}: ${res.rows[i].first_name} ${res.rows[i].last_name}, born ${year}-0${month}-0${date}`);
+    }
+    client.end();
+})
+
