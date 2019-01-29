37b418f38dadb7dda9594914c7a27b94c77386ed
Erin Toth <erintoth@gmail.com>
2019-01-29 21:00:08 +0000

    Separate query and printQuery in to separate functions

--- famous_people.js
+++ famous_people.js
@@ -15,24 +15,52 @@ const data = process.argv.slice(2)[0];
 
 client.connect();
 
-client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [data], (err, res) => {
+
+function fpQuery(data) {
+    client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [data], (err, res) => {
+        if (err) {
+            console.log('ERR', err);
+            return false;
+        }
+        printQuery(data, res.rows);
+    })
+}
+
+function printQuery(data, res) {
     console.log("Searching...");
-    const count = res.rows.length;
+    const count = res.length;
     let num = 1;
-
-    
-    if (err) {
-        console.log('ERR:', err);
-        return false;
-    }
     console.log(`Found ${count} person(s) by the name ${data}:`);
-    for (let i in res.rows) {
-        let date = res.rows[i].birthdate.getDate();
-        let month = res.rows[i].birthdate.getMonth() + 1;
-        let year = res.rows[i].birthdate.getFullYear();
+    for (let i in res) {
+        let date = res[i].birthdate.getDate();
+        let month = res[i].birthdate.getMonth() + 1;
+        let year = res[i].birthdate.getFullYear();
 
-        console.log(`${num++}: ${res.rows[i].first_name} ${res.rows[i].last_name}, born ${year}-0${month}-0${date}`);
+        console.log(`${num++}: ${res[i].first_name} ${res[i].last_name}, born ${year}-0${month}-0${date}`);
     }
     client.end();
-})
+}
+
+fpQuery(data);
+
+// client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [data], (err, res) => {
+//     console.log("Searching...");
+//     const count = res.rows.length;
+//     let num = 1;
+
+    
+//     if (err) {
+//         console.log('ERR:', err);
+//         return false;
+//     }
+//     console.log(`Found ${count} person(s) by the name ${data}:`);
+//     for (let i in res.rows) {
+//         let date = res.rows[i].birthdate.getDate();
+//         let month = res.rows[i].birthdate.getMonth() + 1;
+//         let year = res.rows[i].birthdate.getFullYear();
+
+//         console.log(`${num++}: ${res.rows[i].first_name} ${res.rows[i].last_name}, born ${year}-0${month}-0${date}`);
+//     }
+//     client.end();
+// })
 
